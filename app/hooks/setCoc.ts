f70b4737da81b1b2c9bf0648aff116520a3d10e7
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2024-01-09 20:40:28 +0200

    Update setCoc.ts

--- app/hooks/setCoc.ts
+++ app/hooks/setCoc.ts
@@ -7,7 +7,7 @@ export default function createCookie(data: string) {
     const handletoken = async ()=>{
         try{
 
-          return   await axios.post('https://ts-store-attar.vercel.app/api', {id:`${data}`})
+          return   await axios.post('/api', {id:`${data}`})
 
         }catch(e){
             return e as AxiosError
